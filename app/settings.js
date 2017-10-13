95a439b1399482e4f12d51d48afdd3c9417b27be
Connor <connor.aitken@gmail.com>
2017-10-13 01:39:01 -0700

    added atlas configuration

--- app/settings.js
+++ app/settings.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
     //flowFile: 'flows.json',
-    FlowFile: proccess.env.DEVICEFLOW,
+    FlowFile: process.env.DEVICEFLOW,
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
