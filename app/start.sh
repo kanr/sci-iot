78d34d9a969a91e2c084145a0e6aa6f626e70267
Connor <connor.aitken@gmail.com>
2017-11-10 01:01:51 -0800

    refactoring...

--- app/start.sh
+++ app/start.sh
@@ -7,11 +7,4 @@ mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
 # Start app
-# node-red --settings /usr/src/app/settings.js
-
-# using local electron module instead of the global electron lets you
-# easily control specific version dependency between your app and electron itself.
-# the syntax below starts an X istance with ONLY our electronJS fired up,
-# it saves you a LOT of resources avoiding full-desktops envs
-rm /tmp/.X0-lock &>/dev/null || true
-startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+node-red --settings /usr/src/app/settings.js
